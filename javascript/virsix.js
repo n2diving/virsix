28d87d145885e89d459edb6741e4e8aa0b729f70
n2diving <alexis@gormley-design.com>
2014-11-19 18:04:46 -0800

    went back to 1.2.26 version for routing

--- javascript/virsix.js
+++ javascript/virsix.js
@@ -1,44 +1,38 @@
-// create the module and name it virsixApp
-// also include ngRoute for all our routing needs
 var virsixApp = angular.module('virsixApp', ['ngRoute', 'ui.bootstrap']);
 
-    // configure our routes
-    virsixApp.config(function($routeProvider, $locationProvider) {
-      $routeProvider
+  // router config
+  virsixApp.config(function($routeProvider, $locationProvider) {
+    $routeProvider
 
-            // route for the home page
-            .when('/', {
-              templateUrl : 'pages/home.html',
-              controller  : 'mainController'
-            })
+          .when('/', {
+            templateUrl : 'pages/home.html',
+            controller  : 'mainController'
+          })
 
-            // route for the about page
-            .when('/about', {
-              templateUrl : 'pages/about.html',
-              controller  : 'aboutController'
-            })
+          .when('/games', {
+            templateUrl : 'pages/games.html',
+            controller  : 'gamesController'
+          })
 
-            // route for the contact page
-            .when('/contact', {
-              templateUrl : 'pages/contact.html',
-              controller  : 'contactController'
-            });
-
-            // use the HTML5 History API
-            $locationProvider.html5Mode(true);
-            
+          .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
           });
 
-    // create the controller and inject Angular's $scope
-    virsixApp.controller('mainController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'home';
-      });
+          // use the HTML5 History API
+          $locationProvider.html5Mode(true);
 
-    virsixApp.controller('aboutController', function($scope) {
-      $scope.message = 'about';
+        });
+
+  // controllers
+  virsixApp.controller('mainController', function($scope) {
+      $scope.message = 'home';
     });
 
-    virsixApp.controller('contactController', function($scope) {
-      $scope.message = 'contact';
-    });
+  virsixApp.controller('gamesController', function($scope) {
+    $scope.message = 'games';
+  });
+
+  virsixApp.controller('contactController', function($scope) {
+    $scope.message = 'contact';
+  });
