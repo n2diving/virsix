187070a69b86357fa99ae7eb0bf9c856575473f7
n2diving <alexis@gormley-design.com>
2014-11-19 21:30:15 -0800

    carousel issues

--- javascript/virsix.js
+++ javascript/virsix.js
@@ -37,18 +37,3 @@ var virsixApp = angular.module('virsixApp', ['ngRoute', 'ui.bootstrap', 'angular
     $scope.message = 'contact';
   });
 
-  // virsixApp.controller('carouselController', function ($scope) {
-  //   $scope.myInterval = 5000;
-  //   var slides = $scope.slides = [];
-  //   $scope.addSlide = function() {
-  //     var newWidth = 600 + slides.length + 1;
-  //     slides.push({
-  //       image: ['/images/1000-greatforest-1.jpg', '/images/1000-greatforest-3.jpg', 'images/wallball_3d.png', '/images/1000-greatforest-2.jpg', '/images/1000-greatforest-4.jpg'],
-  //       text: ['More','Extra','Lots of','Surplus', 'more'][slides.length % 5] + ' ' +
-  //         ['Cats', 'Kittys', 'Felines', 'Cutes', 'more'][slides.length % 5]
-  //     });
-  //   };
-  //   for (var i=0; i<5; i++) {
-  //     $scope.addSlide();
-  //   }
-  // });
