11f57453c51daa4f89148e7b15e3895904176f20
n2diving <alexis@gormley-design.com>
2014-11-19 20:54:06 -0800

    trying angular-carousel responsive for mobile

--- javascript/virsix.js
+++ javascript/virsix.js
@@ -1,4 +1,4 @@
-var virsixApp = angular.module('virsixApp', ['ngRoute', 'ui.bootstrap']);
+var virsixApp = angular.module('virsixApp', ['ngRoute', 'ui.bootstrap', 'angular-carousel']);
 
   // router config
   virsixApp.config(function($routeProvider, $locationProvider) {
@@ -37,18 +37,18 @@ var virsixApp = angular.module('virsixApp', ['ngRoute', 'ui.bootstrap']);
     $scope.message = 'contact';
   });
 
-  virsixApp.controller('carouselController', function ($scope) {
-    $scope.myInterval = 5000;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function() {
-      var newWidth = 600 + slides.length + 1;
-      slides.push({
-        image: ['file://Users/agormley/Downloads/VirsixWebsite/Photos/1000-greatforest-1.jpg', 'file://Users/agormley/Downloads/VirsixWebsite/Photos/1000-greatforest-3.jpg', 'file://Users/agormley/Downloads/VirsixWebsite/Photos/wallball_3d.png', 'file://Users/agormley/Downloads/VirsixWebsite/Photos/1000-greatforest-2.jpg', 'file://Users/agormley/Downloads/VirsixWebsite/Photos/1000-greatforest-4.jpg'],
-        text: ['More','Extra','Lots of','Surplus', 'more'][slides.length % 5] + ' ' +
-          ['Cats', 'Kittys', 'Felines', 'Cutes', 'more'][slides.length % 5]
-      });
-    };
-    for (var i=0; i<5; i++) {
-      $scope.addSlide();
-    }
-  });
+  // virsixApp.controller('carouselController', function ($scope) {
+  //   $scope.myInterval = 5000;
+  //   var slides = $scope.slides = [];
+  //   $scope.addSlide = function() {
+  //     var newWidth = 600 + slides.length + 1;
+  //     slides.push({
+  //       image: ['/images/1000-greatforest-1.jpg', '/images/1000-greatforest-3.jpg', 'images/wallball_3d.png', '/images/1000-greatforest-2.jpg', '/images/1000-greatforest-4.jpg'],
+  //       text: ['More','Extra','Lots of','Surplus', 'more'][slides.length % 5] + ' ' +
+  //         ['Cats', 'Kittys', 'Felines', 'Cutes', 'more'][slides.length % 5]
+  //     });
+  //   };
+  //   for (var i=0; i<5; i++) {
+  //     $scope.addSlide();
+  //   }
+  // });
